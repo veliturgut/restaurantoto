353842f4d84cfdce3ced2eb536606253e69dbfdb
Veli Turgut <turgut.v16@gmail.com>
2020-01-22 13:23:14 +0300

    Rezervasyon kısmı ve Odeme kısmı için gerekli metodlar yazıldı

--- restaurantoto/cOdeme.cs
+++ restaurantoto/cOdeme.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace restaurantoto
 {
     class cOdeme
     {
+        cGenel gnl = new cGenel();
 
         #region Fields
         private int _OdemeID;
@@ -79,5 +82,83 @@ namespace restaurantoto
             set { _MusteriId = value; }
         }
         #endregion
+
+
+
+        // Musteri hesap kapatılıyor 
+        public bool billClose(cOdeme bill)
+        {
+            bool result = false;
+
+            SqlConnection con = new SqlConnection(gnl.conString);
+            SqlCommand cmd = new SqlCommand("Insert Into hesapOdemeleri(ADISYONID,ODEMETURID,MUSTERIID,ARATOPLAM,KDVTUTARI,TOPLAMTUTAR,INDIRIM) " +
+                "values (@ADISYONID,@ODEMETURID,@MUSTERIID,@ARATOPLAM,@KDVTUTARI,@TOPLAMTUTAR,@INDIRIM)",con);
+
+            try
+            {
+
+                if(con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                cmd.Parameters.Add("ADISYONID", SqlDbType.Int).Value = bill._AdisyonID;
+                cmd.Parameters.Add("ODEMETURID", SqlDbType.Int).Value = bill._OdemeTurID;
+                cmd.Parameters.Add("MUSTERIID", SqlDbType.Int).Value = bill._MusteriId;
+                cmd.Parameters.Add("ARATOPLAM", SqlDbType.Money).Value = bill._AraToplam;
+                cmd.Parameters.Add("KDVTUTARI", SqlDbType.Money).Value = bill._KdvTutari;
+                cmd.Parameters.Add("INDIRIM", SqlDbType.Money).Value = bill._Indirim;
+                cmd.Parameters.Add("TOPLAMTUTAR", SqlDbType.Money).Value = bill._GenelToplam;
+
+                result = Convert.ToBoolean(cmd.ExecuteNonQuery());
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+
+            }
+            finally
+            {
+                con.Dispose();
+                con.Close();
+            }
+
+            return result;
+
+        }
+
+        // musteri toplam harcama bulunuyor..
+        public decimal sumTotalforClientId(int clientId)
+        {
+            decimal total = 0;
+
+            SqlConnection con = new SqlConnection(gnl.conString);
+
+            SqlCommand cmd = new SqlCommand("Select sum(TOPLAMTUTAR) as total from hesapOdemeleri Where MUSTERIID=@clientId",con);
+
+           
+
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                 cmd.Parameters.Add("clientId", SqlDbType.Int).Value = clientId;
+                 total = Convert.ToDecimal(cmd.ExecuteScalar());
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+                throw;
+            }
+            finally
+            {
+                con.Dispose();
+                con.Close();
+            }
+
+            return total;
+        }
     }
 }
--- restaurantoto/cRezervasyon.cs
+++ restaurantoto/cRezervasyon.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace restaurantoto
 {
     class cRezervasyon
     {
-
+        cGenel gnl = new cGenel();
 
         #region Fields
 
@@ -64,6 +66,46 @@ namespace restaurantoto
             set { _ClientCount = value; }
         }
         #endregion
+
+
+
+        public int getByClientIdFromReservation(int tableId)
+        {
+            int clientId = 0;
+
+            SqlConnection con = new SqlConnection(gnl.conString);
+            SqlCommand cmd = new SqlCommand("Select top 1 MUSTERIID from Rezervasyonlar where MASAID=@masaid order by MUSTERIID Desc", con);
+
+            try
+            {
+
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                cmd.Parameters.Add("masaid", SqlDbType.Int).Value = tableId;
+                
+
+                clientId = Convert.ToInt32(cmd.ExecuteNonQuery());
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+
+            }
+            finally
+            {
+                con.Dispose();
+                con.Close();
+            }
+
+
+
+            return clientId;
+        }
+
+
 
     }
 }
